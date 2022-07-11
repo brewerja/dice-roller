ac27996c95960b07d100a9253763e35f577216ba
John Brewer <brewerja@gmail.com>
2022-07-11 18:52:08 -0400

    Cleanup

--- src/main/java/dieroll/WebSocketConfig.java
+++ src/main/java/dieroll/WebSocketConfig.java
@@ -1,11 +1,7 @@
 package dieroll;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -23,7 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/roll").withSockJS();
-        registry.addEndpoint("/message").withSockJS();
     }
 
 }
