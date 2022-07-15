218067f06eca6f9265d12f1e21314d581ca8255d
John Brewer <brewerja@gmail.com>
2022-07-15 11:47:14 -0400

    Attempt to remove deprecated web security config

--- src/main/java/dieroll/config/WebSecurityConfig.java
+++ src/main/java/dieroll/config/WebSecurityConfig.java
@@ -1,16 +1,20 @@
 package dieroll.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel()
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+                .requiresSecure()
+                .and()
+                .build();
     }
+   
 }
