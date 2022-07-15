d0384f61d094aa4b1a67464dd1584db7bf911c56
John Brewer <brewerja@gmail.com>
2022-07-15 11:06:24 -0400

    Eslint and fix chat

--- src/main/resources/static/dieroll.js
+++ src/main/resources/static/dieroll.js
@@ -1,99 +1,106 @@
 var PLAYER_NAME_KEY = "playerName";
+var rollsClient;
 
 function connectRolls() {
     rollsClient = new StompJs.Client({
-      brokerURL: 'ws://' + window.location.host + '/roll',
-      reconnectDelay: 5000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
+        brokerURL: 'ws://' + window.location.host + '/roll',
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
     });
     rollsClient.onConnect = function(frame) {
-      rollsClient.subscribe('/topic/rolls/' + roomId, function(dieRoll) {
-         showRoll(JSON.parse(dieRoll.body));
-         scrollTop();
-      });
+        rollsClient.subscribe('/topic/rolls/' + roomId, function(dieRoll) {
+            showRoll(JSON.parse(dieRoll.body));
+            scrollTop();
+        });
     }
     rollsClient.activate();
 }
 
 function initializeNames() {
-	if(localStorage.getItem(PLAYER_NAME_KEY)) {
-		$('#name').val(localStorage.getItem(PLAYER_NAME_KEY));
-	}
+    if(localStorage.getItem(PLAYER_NAME_KEY)) {
+        $('#name').val(localStorage.getItem(PLAYER_NAME_KEY));
+    }
 }
 
 function addNameSaveHandlers() {
-	$('#name').change(function(){
-		localStorage.setItem(PLAYER_NAME_KEY, $('#name').val());
-	});
+    $('#name').change(function(){
+        localStorage.setItem(PLAYER_NAME_KEY, $('#name').val());
+    });
 }
 
 $(document).ready(
-		function() {
-            $.get(`/rooms/${roomId}/rolls`, showPriorRolls).done(scrollTop);
-			initializeNames();
-			addNameSaveHandlers();
-			connectRolls();
-			$("#message").val("");
-		});
+    function() {
+        $.get(`/rooms/${roomId}/rolls`, showPriorRolls).done(scrollTop);
+        initializeNames();
+        addNameSaveHandlers();
+        connectRolls();
+        $("#message").val("");
+    });
 
 function showPriorRolls(rolls) {
-  rolls.forEach(showRoll);
-  $("#rollList").find("li").last().attr("class", "list-group-item list-group-item-secondary");
+    rolls.forEach(showRoll);
+    $("#rollList").find("li").last().attr("class", "list-group-item list-group-item-secondary");
 }
 
 function formatTimestamp(timestamp) {
-	return new Date(timestamp).toLocaleString('en-US', { hour12: true });
+    return new Date(timestamp).toLocaleString('en-US', { hour12: true });
 }
 
 function roll(request) {
-	name = $("#name").val();
-	rollsClient.publish({destination: "/app/roll/" + roomId, body: JSON.stringify({
-		'name' : name,
-		'request' : request
-	}),
-	headers: {},
-	});
+    var name = $("#name").val();
+    rollsClient.publish({
+        destination: "/app/roll/" + roomId,
+        body: JSON.stringify({
+            'name' : name,
+            'request' : request
+        }),
+        headers: {},
+    });
 }
 
 var n = 0
 
 $(document).ready(function() {
-	$("#message").keyup(function(e) {
-		if (e.keyCode == 13) {
-			talk();
-			n = 0;
-		} else if (e.keyCode == 38) {
-		    n = Math.max(-100, n - 1);
+    $("#message").keyup(function(e) {
+        if (e.keyCode == 13) {
+            talk();
+            n = 0;
+        } else if (e.keyCode == 38) {
+            n = Math.max(-100, n - 1);
             var lastRequest = $("#rollList").find("li").eq(n).find("span").last().text();
-		    $("#message").val("");
-		    $("#message").val(lastRequest);
-		} else if (e.keyCode == 40) {
-		    n = Math.min(0, n + 1);
-		    $("#message").val("");
-		    if (n != 0) {
+            $("#message").val("");
+            $("#message").val(lastRequest);
+        } else if (e.keyCode == 40) {
+            n = Math.min(0, n + 1);
+            $("#message").val("");
+            if (n != 0) {
                 var lastRequest = $("#rollList").find("li").eq(n).find("span").last().text();
                 $("#message").val(lastRequest);
             }
-		}
-	});
+        }
+    });
 });
 
 function talk() {
-	name = $("#name").val();
-	message = $("#message").val();
-	if (message == "")
-	    return
-	rollsClient.send("/app/roll/" + roomId, {}, JSON.stringify({
-		'name' : name,
-		'request' : message,
-	}));
-	$("#message").val("");
+    var name = $("#name").val();
+    var message = $("#message").val();
+    if (message == "")
+        return
+    rollsClient.publish({
+        destination: "/app/roll/" + roomId,
+        body: JSON.stringify({
+            'name' : name,
+            'request' : message,
+        }),
+        headers: {}}
+    );
+    $("#message").val("");
 }
 
 function scrollTop() {
-	var rollContainer = document.getElementById('rollContainer');
-	rollContainer.scrollTop = rollContainer.scrollHeight;
+    var rollContainer = document.getElementById('rollContainer');
+    rollContainer.scrollTop = rollContainer.scrollHeight;
 }
 
 function getRequestDisplay(request, results) {
@@ -113,14 +120,14 @@ function showRoll(roll) {
     ul.find("li").last().attr("class", "list-group-item list-group-item-secondary");
     if (roll.results == null) {
         ul.append(`<li class="list-group-item list-group-item-primary" title="${formatTimestamp(roll.timestamp)}">
-                   <span class="me-2">${roll.name}:</span>
-                   <span>${roll.request}</span>
-                   </li>`);
+<span class="me-2">${roll.name}:</span>
+<span>${roll.request}</span>
+</li>`);
     } else {
         ul.append(`<li class="list-group-item list-group-item-primary" title="${formatTimestamp(roll.timestamp)}">
-                   <span class="me-2">${roll.name}</span>
-                   ${getRequestDisplay(roll.request, roll.results)}
-                   <span class="fs-6 fst-italic">${roll.request}</span>
-                   </li>`);
+<span class="me-2">${roll.name}</span>
+${getRequestDisplay(roll.request, roll.results)}
+<span class="fs-6 fst-italic">${roll.request}</span>
+</li>`);
     }
 }
